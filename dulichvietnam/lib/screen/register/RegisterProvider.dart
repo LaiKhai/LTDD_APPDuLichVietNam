075b4c2755a3f3971ca7833d51fcaf4346584f05
Lai Khải <56122788+LaiKhai@users.noreply.github.com>
2022-01-05 22:03:08 +0700

    update_register

--- dulichvietnam/lib/screen/register/RegisterProvider.dart
+++ dulichvietnam/lib/screen/register/RegisterProvider.dart
@@ -1,1 +1,34 @@
-class RegisterProvider {}
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class RegisterProvider {
+  register(String email, password, confirm_password, name) async {
+    Map data = {
+      'email': email,
+      'password': password,
+      'confirm_password': confirm_password,
+      'hoten': name,
+    };
+    print(data);
+
+    String body = json.encode(data);
+    var url = 'http://192.168.1.5:80/api/register';
+    var response = await http.post(
+      Uri.parse(url),
+      body: body,
+      headers: {
+        "Content-Type": "application/json",
+        "accept": "application/json",
+        "Access-Control-Allow-Origin": "*"
+      },
+    );
+    print(response.body);
+    print(response.statusCode);
+    if (response.statusCode == 200) {
+      //Or put here your next screen using Navigator.push() method
+      print('success');
+    } else {
+      print('error');
+    }
+  }
+}
--- dulichvietnam/test/widget_test.dart
+++ dulichvietnam/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:dulichvietnam/main.dart';
+import 'package:VietNamTravel/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
